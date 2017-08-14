a219dff12f054c9c514581cd392971c74c461155
Edinwell <moderato11@gmail.com>
2017-08-14 19:34:53 +0900

    Update gstk.py

--- gstk.py
+++ gstk.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 style.use('ggplot')
 
-quandl.ApiConfig.api_key = 'suVmsE-rKX_Vnxc5EKFA'
+quandl.ApiConfig.api_key = '[fill your own api_key]'
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume', ]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
